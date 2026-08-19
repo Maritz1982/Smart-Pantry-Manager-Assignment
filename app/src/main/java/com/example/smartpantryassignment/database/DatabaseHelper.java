493a7e7bd4c2ff7b39ba90982cc6a3c5e73150be
maritza <maritzavz.m@gmail.com>
2026-08-19 17:13:11 +0200

    Create pantry database table and retrieval method

--- app/src/main/java/com/example/smartpantryassignment/database/DatabaseHelper.java
+++ app/src/main/java/com/example/smartpantryassignment/database/DatabaseHelper.java
@@ -1,8 +1,11 @@
 package com.example.smartpantryassignment.database;
+import com.example.smartpantryassignment.models.PantryItem;
 import android.content.Context;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import java.util.ArrayList;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -20,17 +23,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                         "ingredient TEXT," +
                         "quantity REAL," +
                         "unitMeasure TEXT," +
-                        "expiryDate TEXT" +
+                        "expireDate TEXT" +
                         ")";
     db.execSQL(CREATE_PANTRY_TABLE);
     } //creates table in the SQL database
     public void addIngredient(PantryItem item) {
-        SQLiteDatabase db = this.getWritetableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("ingredient", item.getIngredient());
         values.put("quantity", item.getQuantity());
-        values.put("unitMeasure", item.getunitMeasure());
-        values.put("expireDate", item.getexpireDate());
+        values.put("unitMeasure", item.getUnitMeasure());
+        values.put("expireDate", item.getExpireDate());
 
         db.insert("PantryItems", null, values);
         db.close();
@@ -38,6 +41,38 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
     }
+    public ArrayList<PantryItem> getAllIngredients() { //add method
+        ArrayList<PantryItem> pantryList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM PantryItems",
+                null);
+        if (cursor.moveToFirst()) {
+            do {
+                PantryItem item = new PantryItem();
+                item.setPantryId(
+                        cursor.getInt(0));
+                item.setIngredient(
+                        cursor.getString(1));
+                item.setQuantity(
+                        cursor.getDouble(2));
+                item.setUnitMeasure(
+                        cursor.getString(3));
+                item.setExpireDate(
+                        cursor.getString(4));
+                pantryList.add(item);
+            } while (cursor.moveToNext());
+
+        }
+        cursor.close();
+        db.close();
+        return pantryList;
+
+    }
 }
+
+
+
+
 
 //database helper manages the SQLite database where it will create tables and CRUD methods
