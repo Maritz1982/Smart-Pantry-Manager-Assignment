c2909a47b7c600b270f3b3a71f60b878524846bf
maritza <maritzavz.m@gmail.com>
2026-08-22 09:35:01 +0200

    Implement recipe matching algorithm

--- app/src/main/java/com/example/smartpantryassignment/database/DatabaseHelper.java
+++ app/src/main/java/com/example/smartpantryassignment/database/DatabaseHelper.java
@@ -121,98 +121,103 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return pantryList;
 
     }
-public void addRecipe(String recipeName, //getting the addrecipe's
-                      String recipeProcess){
+
+    public void addRecipe(String recipeName, //getting the addrecipe's
+                          String recipeProcess) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("recipeName", recipeName);
         values.put("recipeProcess", recipeProcess);
         db.insert("Recipes", null, values);
         db.close();
-}
-public void seedRecipes() {
-    addRecipe(
-            "Pancakes",
-            "Mix all dry ingredients first. Then add the wet ingredients and mix thoroughly. Cook in high heat pan. Turn once little bubbles appear. Serve with choice of sugar or savoury toppings."
-    );
-    addRecipe("French Toast",
-            "Mix eggs with salt and pepper, use a fork to mix for best results. Dip the bread in the egg mixture and fry. Serve with syrup or butter"
-    );
-    addRecipe("Omelette",
-            "Mix eggs with milk, beat mix and add salt. Fry in pan until slightly stiffened then turn."
-    );
-    addRecipe("Toasted Cheese and Tomato",
-            "Butter bread and turn upside down(butter should be on the outside). Put thinly sliced tomato, spice with favourite spices. Add cheese genorously. Add sweetchilly and mayonaise drops all over. Close the sandwich and bake for 3 mins on each side in the airfryer"
-    );
-    addRecipe(
-            "Egg Fried Rice",
-            "Cook rice with the egg mixture and cut vegatables."
-    );
-}
-public void addRecipeIngredient(int recipeId,
-                                String ingredient,
-                                double quantity,
-                                String unitMeasure) {
-    SQLiteDatabase db = this.getWritableDatabase();
-    ContentValues values = new ContentValues();
-    values.put("recipeId", recipeId);
-    values.put("quantity", quantity);
-    values.put("unitMeasure", unitMeasure);
-    values.put("recipeId", recipeId);
-    values.put("ingredient", ingredient);
-    values.put("quantity", quantity);
-    values.put("unitmeasure", unitMeasure);
-    db.insert("RecipeIngredients", null, values);
-    db.close();
-}
-public void seedRecipeIngredients() {
-    // Pancake recipe ID 1
-    addRecipeIngredient(1,
-            "Eggs",
-            2,
-            "pieces");
-    addRecipeIngredient(1,
-            "Milk",
-            250,
-            "ml");
-    addRecipeIngredient(1,
-            "Flour",
-            200,
-            "g");
-    //French toast recipe ID 2
-    addRecipeIngredient(2,
-            "Eggs",
-            2,
-            "pieces");
-    addRecipeIngredient(2,
-            "Milk",
-            100,
-            "ml");
-    addRecipeIngredient(2,
-            "Flour",
-            25,
-            "g");
-    addRecipeIngredient(2,
-            "Bread",
-            2,
-            "pieces");
-    // Omelette Recipe DI 3
-    addRecipeIngredient(3,
-            "Eggs",
-            3,
-            "pieces");
-    addRecipeIngredient(3,
-            "Milk",
-            50,
-            "ml");
-}
-    public ArrayList<Recipe> getAllRecipes(){ //db retrieval methods
+    }
+
+    public void seedRecipes() {
+        addRecipe(
+                "Pancakes",
+                "Mix all dry ingredients first. Then add the wet ingredients and mix thoroughly. Cook in high heat pan. Turn once little bubbles appear. Serve with choice of sugar or savoury toppings."
+        );
+        addRecipe("French Toast",
+                "Mix eggs with salt and pepper, use a fork to mix for best results. Dip the bread in the egg mixture and fry. Serve with syrup or butter"
+        );
+        addRecipe("Omelette",
+                "Mix eggs with milk, beat mix and add salt. Fry in pan until slightly stiffened then turn."
+        );
+        addRecipe("Toasted Cheese and Tomato",
+                "Butter bread and turn upside down(butter should be on the outside). Put thinly sliced tomato, spice with favourite spices. Add cheese genorously. Add sweetchilly and mayonaise drops all over. Close the sandwich and bake for 3 mins on each side in the airfryer"
+        );
+        addRecipe(
+                "Egg Fried Rice",
+                "Cook rice with the egg mixture and cut vegatables."
+        );
+    }
+
+    public void addRecipeIngredient(int recipeId,
+                                    String ingredient,
+                                    double quantity,
+                                    String unitMeasure) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("recipeId", recipeId);
+        values.put("quantity", quantity);
+        values.put("unitMeasure", unitMeasure);
+        values.put("recipeId", recipeId);
+        values.put("ingredient", ingredient);
+        values.put("quantity", quantity);
+        values.put("unitmeasure", unitMeasure);
+        db.insert("RecipeIngredients", null, values);
+        db.close();
+    }
+
+    public void seedRecipeIngredients() {
+        // Pancake recipe ID 1
+        addRecipeIngredient(1,
+                "Eggs",
+                2,
+                "pieces");
+        addRecipeIngredient(1,
+                "Milk",
+                250,
+                "ml");
+        addRecipeIngredient(1,
+                "Flour",
+                200,
+                "g");
+        //French toast recipe ID 2
+        addRecipeIngredient(2,
+                "Eggs",
+                2,
+                "pieces");
+        addRecipeIngredient(2,
+                "Milk",
+                100,
+                "ml");
+        addRecipeIngredient(2,
+                "Flour",
+                25,
+                "g");
+        addRecipeIngredient(2,
+                "Bread",
+                2,
+                "pieces");
+        // Omelette Recipe DI 3
+        addRecipeIngredient(3,
+                "Eggs",
+                3,
+                "pieces");
+        addRecipeIngredient(3,
+                "Milk",
+                50,
+                "ml");
+    }
+
+    public ArrayList<Recipe> getAllRecipes() { //db retrieval methods
         ArrayList<Recipe> recipeList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(
                 "SELECT * FROM Recipes",
                 null);
-        if (cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             do {
                 Recipe recipe = new Recipe();
                 recipe.setRecipeId(
@@ -226,12 +231,12 @@ public void seedRecipeIngredients() {
 
         }
         cursor.close();
-    db.close();
-    return recipeList;
+        db.close();
+        return recipeList;
     }
 
     public ArrayList<RecipeIngredient>
-    getIngredientsForRecipe(int recipeId){
+    getIngredientsForRecipe(int recipeId) {
         ArrayList<RecipeIngredient> ingredientList =
                 new ArrayList<>();
         SQLiteDatabase db =
@@ -240,7 +245,7 @@ public void seedRecipeIngredients() {
                 "SELECT * FROM RecipeIngredients WHERE recipeId=?",
                 new String[]{String.valueOf(recipeId)}
         );
-        if (cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             do {
                 RecipeIngredient ingredient =
                         new RecipeIngredient();
@@ -253,14 +258,51 @@ public void seedRecipeIngredients() {
                 ingredient.setUnitMeasure(
                         cursor.getString(4));
                 ingredientList.add(ingredient);
-            } while(cursor.moveToNext());
+            } while (cursor.moveToNext());
 
-            }
+        }
         cursor.close();
         db.close();
         return ingredientList;
+    }
+
+    public ArrayList<Recipe> getSuggestedRecipes() { //strict matching logic creation
+        ArrayList<Recipe> suggestedRecipes =
+                new ArrayList<>();
+        ArrayList<Recipe> recipes = getAllRecipes();
+        for (Recipe recipe : recipes) {
+            boolean canMake = true;
+            ArrayList<RecipeIngredient> ingredients =
+                    getIngredientsForRecipe(
+                            recipe.getRecipeId());
+            for (RecipeIngredient ingredient : ingredients) {
+                boolean found = false;
+                ArrayList<PantryItem> pantryItems =
+                        getAllIngredients();
+                for (PantryItem pantryItem : pantryItems) {
+                    if (pantryItem.getIngredient()
+                            .equalsIgnoreCase(
+                                    ingredient.getIngredient())) {
+                        if (pantryItem.getQuantity()
+                                >= ingredient.getQuantity()) {
+                            found = true;
+                        }
+                    }
+                }
+                if (!found){
+                    canMake = false;
+                    break;
+                }
+            }
+            if (canMake){
+                suggestedRecipes.add(recipe);
+            }
         }
+        return suggestedRecipes;
+    }
 }
+
+
 
 
 
